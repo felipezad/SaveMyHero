ce86e171ef86ebde7d97d359b9709f58e6b97c67
Felipe Zadinello <felipe.zadinello@arctouch.com>
2020-05-16 13:56:31 -0300

    Setup mock network module

---
+++ app/src/main/java/com/exercise/savemyhero/data/remote/NetworkModule.kt
@@ -0,0 +1,53 @@
+package com.exercise.savemyhero.data.remote
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.*
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    @JvmStatic
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    @JvmStatic
+    fun provideOkHttp(): OkHttpClient {
+        return OkHttpClient()
+            .newBuilder()
+            .addInterceptor(ApiInterceptor())
+            .build()
+    }
+
+    private class ApiInterceptor : Interceptor {
+
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request()
+            val urlBuilder = request.url().newBuilder()
+
+            val newUrl = urlBuilder
+                .addQueryParameter("app_id", "foo")
+                .addQueryParameter("app_key", "boo")
+                .build()
+
+            val newRequest = request.buildNewUrl(newUrl)
+
+            return chain.proceed(newRequest)
+        }
+
+        private fun Request.buildNewUrl(newUrl: HttpUrl): Request {
+            return this.newBuilder().url(newUrl).build()
+        }
+    }
+}
