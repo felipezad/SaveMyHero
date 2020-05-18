679e0019b32e0b8a7c54f8aa7fa9c90378f3dce6
Felipe Zadinello <felipe.zadinello@arctouch.com>
2020-05-17 22:14:41 -0300

    Remove empty lines

--- app/src/main/java/com/exercise/savemyhero/data/remote/model/ApiResponses.kt
+++ app/src/main/java/com/exercise/savemyhero/data/remote/model/ApiResponses.kt
@@ -3,7 +3,6 @@ package com.exercise.savemyhero.data.remote.model
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-
 @JsonClass(generateAdapter = true)
 data class ApiResponse<out T>(
     @Json(name = "attributionHTML")
